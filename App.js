c8455306c3f6a40a9acd965408aadb0e2e3c902e
Konstantin Savchenko <killerkennyx41@gmail.com>
2019-06-26 21:08:31 +0300

    Search Page without search

--- App.js
+++ App.js
@@ -1,79 +1,128 @@
-import React from 'react'
-import { View, ScrollView, Button } from 'react-native'
-import { createStackNavigator, createAppContainer } from "react-navigation"
-import { Header, ImageCard, Layout, Details } from './components/common'
+import React from 'react';
+import { View, ScrollView } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { Header, ImageCard, Layout, Details } from './components/common';
 
-const clientId = 'ad37002fd54b270f4db76a58f2d261f8f8826507fd09dcd518160738411f8c2e'
-const endpoint = 'https://api.unsplash.com/photos'
+const clientId = 'ad37002fd54b270f4db76a58f2d261f8f8826507fd09dcd518160738411f8c2e';
+const endpoint = 'https://api.unsplash.com/photos';
 
 class HomeScreen extends React.Component {
   state = {
     headerTitle: 'Photos Galary',
-    photos: []
-  }
-  
+    photos: [],
+  };
+
   componentDidMount() {
     fetch(`${endpoint}?client_id=${clientId}`)
       .then(res => res.json())
-      .catch(() => []) 
-      .then(photos => this.setState({photos}))
+      .catch(() => [])
+      .then(photos => this.setState({ photos }));
   }
 
   render() {
-    const { headerTitle } = this.state
-    const { photos } = this.state
-    const { navigation } = this.props
+    const { headerTitle } = this.state;
+    const { photos } = this.state;
+    const { navigation } = this.props;
+
+    console.log(navigation);
 
     return (
       <View>
-        <Header title={headerTitle} />
+        <Header
+          title={headerTitle}
+          searchIcon="search"
+          onPress={() => navigation.navigate('Search')}
+        />
         <ScrollView>
           <Layout>
             {photos.map(item => (
-              <ImageCard 
+              <ImageCard
                 key={item.id}
                 title={item}
-                onPress={() => navigation.navigate('Details', (item))}
+                onPress={() => navigation.navigate('Details', item)}
               />
             ))}
           </Layout>
         </ScrollView>
       </View>
-    )
+    );
+  }
+}
+
+class SearchScreen extends React.Component {
+  state = {
+    photos: [],
+  };
+
+  componentDidMount() {
+    fetch(`${endpoint}?client_id=${clientId}`)
+      .then(res => res.json())
+      .catch(() => [])
+      .then(photos => this.setState({ photos }));
+  }
+
+  render() {
+    const { photos } = this.state;
+    const { navigation } = this.props;
+
+    console.log(photos);
+
+    return (
+      <View>
+        <Header
+          goBackIcon="chevron-left"
+          searchIcon="search"
+          onPressBack={() => navigation.goBack()}
+          onPressSearch={() => navigation.goBack()}
+        />
+        <ScrollView>
+          <Layout>
+            {photos.map(item => (
+              <ImageCard
+                key={item.id}
+                title={item}
+                onPress={() => navigation.navigate('Details', item)}
+              />
+            ))}
+          </Layout>
+        </ScrollView>
+      </View>
+    );
   }
 }
 
 export class DetailsScreen extends React.Component {
   state = {
-    headerTitle: 'Details Page'
-  }
+    headerTitle: 'Details Page',
+  };
 
   render() {
-    const { headerTitle } = this.state
-    
+    const { headerTitle } = this.state;
+
     return (
       <View>
-        <Header title={headerTitle} />
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
+        <Header
+          title={headerTitle}
+          goBackIcon="chevron-left"
+          onPressBack={() => this.props.navigation.goBack()}
         />
         <ScrollView>
-          <Details title={this.props.navigation.state.params}/>
+          <Details title={this.props.navigation.state.params} />
         </ScrollView>
       </View>
-    )
+    );
   }
 }
 
 const AppNavigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Details: DetailsScreen
+    Details: DetailsScreen,
+    Search: SearchScreen,
   },
   {
-    initialRouteName: "Home",
-    headerMode: 'none'
+    initialRouteName: 'Home',
+    headerMode: 'none',
   }
 );
 
--- components/common/Header.js
+++ components/common/Header.js
@@ -1,14 +1,43 @@
 // Import
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 import Constants from 'expo-constants';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Entypo from 'react-native-vector-icons/Entypo';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 // Body
-const Header = ({ title }) => {
-  const { headerContainer, headerText } = styles;
+const Header = ({ title, searchIcon, goBackIcon, onPress, onPressBack, onPressSearch }) => {
+  const {
+    headerContainer,
+    titleContainer,
+    headerText,
+    goBackIconStyle,
+    searchIconStyle,
+    searchArea,
+  } = styles;
   return (
     <View style={headerContainer}>
-      <Text style={headerText}>{title}</Text>
+      <View style={titleContainer}>
+        {goBackIcon && (
+          <TouchableOpacity onPress={onPressBack}>
+            <Entypo name={goBackIcon} style={goBackIconStyle} />
+          </TouchableOpacity>
+        )}
+        <Text style={headerText}>{title}</Text>
+      </View>
+      {searchIcon && (
+        <TouchableOpacity onPress={onPress}>
+          <FontAwesome name={searchIcon} style={searchIconStyle} />
+        </TouchableOpacity>
+      )}
+      {onPressSearch && (
+        <TouchableOpacity onPress={onPress}>
+          <View style={searchArea}>
+            <TextInput placeholder="Search" />
+          </View>
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
@@ -17,7 +46,9 @@ const Header = ({ title }) => {
 const styles = StyleSheet.create({
   headerContainer: {
     height: 80,
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
     paddingTop: Constants.statusBarHeight,
     backgroundColor: '#4bcffc',
     shadowColor: '#000',
@@ -26,12 +57,34 @@ const styles = StyleSheet.create({
     elevation: 8,
     position: 'relative',
   },
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   headerText: {
-    margin: 24,
+    margin: 20,
     fontSize: 28,
     color: '#fff',
     fontWeight: 'bold',
-    textAlign: 'center',
+  },
+  goBackIconStyle: {
+    marginLeft: 10,
+    marginRight: -10,
+    fontSize: 35,
+    color: '#fff',
+  },
+  searchIconStyle: {
+    marginRight: 20,
+    fontSize: 25,
+    color: '#fff',
+  },
+  searchArea: {
+    backgroundColor: '#fff',
+    marginRight: 20,
+    width: 210,
+    padding: 5,
+    borderRadius: 5,
+    height: 30,
   },
 });
 
