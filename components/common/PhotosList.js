7899e11c37932ffee967b6ea9f70ef279781a988
Konstantin Savchenko <killerkennyx41@gmail.com>
2019-07-04 00:49:16 +0300

    Export/import resolve

--- components/common/PhotosList.js
+++ components/common/PhotosList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text } from 'react-native';
-import { ImageCard } from './components/common';
+import { ImageCard } from './';
 // import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 import { connect } from 'react-redux';
--- components/common/index.js
+++ components/common/index.js
@@ -2,4 +2,3 @@ export * from './Header';
 export * from './ImageCard';
 export * from './Layout';
 export * from './Details';
-export * from './PhotosList';
